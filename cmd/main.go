67409e677e00b4e805f75493405c2dece6136a5c
Pablo <pablomokorel@gmail.com>
2024-06-10 16:13:07 -0300

    Se agrega lector basico de CSV

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	CSVPgen "CSVPgen/internal/csv"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("CSVP&gen")
+
+	filePath := "data/input/data_test.csv"
+
+	fmt.Println(CSVPgen.ReadCSV(filePath))
+
 }
--- internal/csv/reader.go
+++ internal/csv/reader.go
@@ -1,1 +1,48 @@
 package csv
+
+import (
+	"encoding/csv"
+	"os"
+	//"path/filepath"
+	//"strings"
+)
+
+// Campo de dato
+type StructField struct {
+	Name  string
+	Value string
+}
+
+// Fila del csv
+type Row struct {
+	Fields []StructField
+}
+
+func ReadCSV(filepath string) ([]Row, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	rows, err := reader.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	columnNames := rows[0]
+
+	var processedRows []Row
+
+	for _, row := range rows[1:] {
+		var fields []StructField
+		for i, value := range row {
+			fields = append(fields, StructField{Name: columnNames[i], Value: value})
+		}
+		processedRows = append(processedRows, Row{Fields: fields})
+	}
+
+	return processedRows, nil
+}
